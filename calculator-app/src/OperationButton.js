d98ee1839ed9abac5c9b9204ebfdbed15c03f8e4
kuriakosant <kuriakosant2003@gmail.com>
2024-10-01 12:00:00 +0300

    add clear action

--- calculator-app/src/OperationButton.js
+++ calculator-app/src/OperationButton.js
@@ -1,7 +1,7 @@
 import { ACTIONS } from "./App"
 
 export default function OperationButton({dispatch, operation}) {
-    return <button onClick={() => dispatch({type: ACTIONS.OPERATION, payload: {operation}})}
+    return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: {operation}})}
     
     >
         {operation}
