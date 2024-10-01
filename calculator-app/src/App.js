79e1a1ed753d77860d6630563ca02522920b5d2b
kuriakosant <kuriakosant2003@gmail.com>
2024-10-01 12:00:00 +0300

    create function : evaluate for current and previous operands

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -43,11 +43,43 @@ function reducer(state, {type , payload}) {
           }
         }
 
+        return{
+          ...state,
+          previousOperand: evaluate(state),
+          operation: payload.operation,
+          currentOperand: null
+        }
+
       case ACTIONS.CLEAR:
         return{}
   }
   
 }
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if (isNaN(prev) || isNaN(current)) return ""
+  let computation = ""
+  switch (operation) {
+    case "+":
+      computation = prev + current
+      break
+    case "-":
+      computation = prev - current
+      break
+    case "*":
+      computation = prev * current
+      break
+    case "÷":
+      computation = prev / current
+      break
+  }
+
+  return computation.toString()
+}
+
+
 
 function App() {
   
