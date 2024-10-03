11473fac18c1b726b40f2530177830f1534a370e
kuriakosant <kuriakosant2003@gmail.com>
2024-10-03 12:00:00 +0300

    update and fix digit button component

--- calculator-app/src/DigitButton.js
+++ calculator-app/src/DigitButton.js
@@ -1,10 +1,11 @@
 import { ACTIONS } from "./App"
 
-export default function OperationButton({dispatch, operation}) {
-    return <button onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit}})}
-    
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
     >
-        {digit}
-        </button>
-    
+      {digit}
+    </button>
+  )
 }
