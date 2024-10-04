aeff2ec059be528c36bd3846ba8b391da52cbbb7
kuriakosant <kuriakosant2003@gmail.com>
2024-10-04 12:00:00 +0300

    add formatOperand function

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -125,6 +125,14 @@ const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
   maximumFractionDigits: 0,
 })
 
+function formatOperand(operand) {
+  if (operand == null) return
+  const [integer, decimal] = operand.split(".")
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
+
+
 
 function App() {
   
